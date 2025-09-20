11de300393536816565014a5ac5154ca58f59fe8
Wp_devsite <leshiy_pos@mail.ru>
2025-09-20 08:35:03 +0300

    Fix preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,5 @@
 // preloader
 
-if (document.getElementById("preloader")) {
-  document.body.style.overflow = "hidden";
-  const loader = () => {
-    document.body.style.overflow = "";
-    const preloader = document.getElementById("preloader");
-    const fadeout = setInterval(() => {
-      const opacity = getComputedStyle(preloader).opacity;
-      opacity > 0
-        ? (preloader.style.opacity = opacity - 0.1)
-        : (clearInterval(fadeout), preloader.remove());
-    }, 15);
-  };
-
-  setTimeout(() => loader(), 2000);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   // preloader
 
@@ -35,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => loader(), 2000);
   }
   // конец preloader
+
   function cloneWidth(parentSelector, childsSelector) {
     const parent = document.querySelector(parentSelector);
     const children = document.querySelectorAll(childsSelector);
