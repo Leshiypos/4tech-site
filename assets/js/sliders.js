2174568af3af3525ebb2849c963bd59fc09961ef
Wp_devsite <leshiy_pos@mail.ru>
2025-09-21 14:05:09 +0300

    test

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -252,7 +252,7 @@ document.addEventListener("DOMContentLoaded", () => {
           anticipatePin: 1,
           pinSpacing: true,
           invalidateOnRefresh: true,
-          //   markers: true,
+          markers: true,
         },
       });
 
@@ -274,10 +274,11 @@ document.addEventListener("DOMContentLoaded", () => {
       };
     });
   }
-  fadeInAnimation(".fade_in");
+
   initOveryoneHere();
   gorizontalSwiper();
   partnersGorizontalSliders();
+  fadeInAnimation(".fade_in");
 
   //   Секция наши клиенты
   let our_clients = document.querySelector(".our_clients_slider");
